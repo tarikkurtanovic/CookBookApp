10d6af64a48c9a5276c94e7ad0c4cf2a5e6e7aa7
tarikkurtanovic <tarik2371@gmail.com>
2023-05-06 16:53:45 +0200

    Initial commit

---
+++ src/js/views/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const PAGE_PAGGANATION = 1;
+export const KEY = '221b6e6a-2ff1-4e3b-aca9-5a8119059a65';
+export const MODAL_CLOSE_SEC = 2.5;
